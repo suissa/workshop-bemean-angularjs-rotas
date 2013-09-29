86510681bebebed644be157db6e1d7f0a17a31b1
Suissa <jnascimento@gmail.com>
2013-09-29 15:17:50 -0300

    Mostrando array de cervejas e usando ng-repeat na view para montar uma lista

--- js/controllers.js
+++ js/controllers.js
@@ -10,5 +10,9 @@ angular.module('myApp.controllers', []).
 
   }])
   .controller('BeerController', function($scope, $http) {
-    $scope.cerveja = "Baden Baden";
+
+    var cervejas = [{name: "Cerveja 01", type: "Cerveja 01", description: "Cerveja 01"}, 
+    {name: "Cerveja 02", type: "Cerveja 02", description: "Cerveja 02"},
+    {name: "Cerveja 03", type: "Cerveja 03", description: "Cerveja 03"}];
+    $scope.cervejas = cervejas;
   });
